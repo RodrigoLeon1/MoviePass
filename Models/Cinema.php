3ed25e36c8682c5f1a918e817986bd376f5961f6
Rodrigo <Leon>
2019-11-15 19:35:17 -0300

    fix3

---
+++ Models/Cinema.php
@@ -0,0 +1,41 @@
+<?php
+
+    namespace Models;
+
+    class Cinema {
+
+        private $id;
+        private $name;        
+        private $address;
+
+        public function getId() {
+            return $this->id;
+        }
+
+        public function setId($id) {
+            $this->id = $id;
+            return $id;
+        }
+
+		public function getName() {
+            return $this->name;
+        }
+
+        public function setName($name) {
+            $this->name = $name;
+            return $this;
+        }
+
+
+        public function getAddress() {
+            return $this->address;
+        }
+
+        public function setAddress($address) {
+            $this->address = $address;
+            return $this;
+        }
+
+    }
+
+?>
